929134b612b91bbdc13a743b57b9fcad2f830c4f
Mansi112 <rajawatmansi042@gmail.com>
2025-03-26 23:38:55 +0530

    Arithematic operator

---
+++ Arithematic.java
@@ -0,0 +1,15 @@
+package Operators;
+import java.util.Scanner;
+public class Arithematic {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter the numbers:");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication:" + (a * b));
+        System.out.println("Division: " + (a / b));
+        System.out.println("Modulus: " + (a % b));
+    }
+}

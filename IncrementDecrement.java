b1eef2a3859beb4cdc60cdef4d5d4e93baaf6e69
Mansi112 <rajawatmansi042@gmail.com>
2025-03-27 00:24:17 +0530

    IncrementDecrement

---
+++ IncrementDecrement.java
@@ -0,0 +1,14 @@
+package Operators;
+
+public class IncrementDecrement {
+    public static void main(String[] args) {
+       int a = 10;
+        System.out.println(a);
+        System.out.println(a++);
+        System.out.println(++a);
+
+        // decrement
+        System.out.println(a--);
+        System.out.println(--a);
+    }
+}

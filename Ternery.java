7f5e8fa13ac812e89b0783eab4a268370878687f
Mansi112 <rajawatmansi042@gmail.com>
2025-03-27 21:02:13 +0530

    Ternery operator

---
+++ Ternery.java
@@ -0,0 +1,10 @@
+package Operators;
+
+public class Ternery {
+    public static void main(String[] args) {
+        int a = 13;
+        int b= 34;
+        int max = (a > b) ? a : b;  // by using question  mark it indicates that we are asking for compiler according to the condition
+        System.out.println(max); // print the greater number .
+    }
+}
